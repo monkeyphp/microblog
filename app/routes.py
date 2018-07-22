5d6b22642b6d4f48bb25df634be873ff3669b705
David White <david@monkeyphp.com>
2018-07-22 15:56:23 +0100

    chapter 2 addded templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'David'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in West Yorkshire!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so uncool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
