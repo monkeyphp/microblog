183f75850b95f338c8087ec720aabfbe0d366950
David White <david@monkeyphp.com>
2018-07-27 17:22:12 +0100

    chapter 12 dates and times

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
